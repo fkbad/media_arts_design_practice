b8125c9f3e8681c8c97777d2a46a6707cc6f6aa2
fkbad <fkb@uchicago.edu>
2023-04-12 08:42:43 -0500

    managed to get chopping into Nth's working

--- do_it_wrong_gay/gay_lib.sh
+++ do_it_wrong_gay/gay_lib.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 #gay() {
   #echo "$1" | gay -g
 #}
@@ -16,17 +17,17 @@
 # https://stackoverflow.com/questions/18560647/how-to-make-bash-shell-display-colorful-text
 
 # figuring out colored text
-echo
-echo -e "$(tput bold) reg  bld  und   tput-command-colors$(tput sgr0)"
+# echo
+# echo -e "$(tput bold) reg  bld  und   tput-command-colors$(tput sgr0)"
 
-for i in $(seq 1 7); do
-  echo " $(tput setaf $i)Text$(tput sgr0) $(tput bold)$(tput setaf $i)Text$(tput sgr0) $(tput sgr 0 1)$(tput setaf $i)Text$(tput sgr0)  \$(tput setaf $i)"
-done
+# for i in $(seq 1 7); do
+#   echo " $(tput setaf $i)Text$(tput sgr0) $(tput bold)$(tput setaf $i)Text$(tput sgr0) $(tput sgr 0 1)$(tput setaf $i)Text$(tput sgr0)  \$(tput setaf $i)"
+# done
 
-echo ' Bold            $(tput bold)'
-echo ' Underline       $(tput sgr 0 1)'
-echo ' Reset           $(tput sgr0)'
-echo
+# echo ' Bold            $(tput bold)'
+# echo ' Underline       $(tput sgr 0 1)'
+# echo ' Reset           $(tput sgr0)'
+# echo
 
 
 #================================================== 
@@ -36,20 +37,20 @@ echo
 bold_color_text() {
   echo " $(tput setaf $1) $(tput bold)"
 }
-blue_bold() {
+blue() {
   bold_color_text "45"
 }
-purple_bold() {
+purple() {
   bold_color_text "5"
 }
-gray_bold() {
+gray() {
   bold_color_text "7"
 }
-pink_bold() {
+pink() {
   bold_color_text "219"
 }
 
-white_bold() {
+white() {
   bold_color_text "15"
 }
 # tests
@@ -60,15 +61,18 @@ white_bold() {
 # pink_bold "gaygay"
 
 
+# 2 argument needed
+#   $1: the number of prints per cycle
+#   $2: actual text to print per line 
+# will inherit whatever the previously set color is
 cycle() {
-  echo
-  echo "beginning:"
+  clear
   switch=0
-  for ((i = 1; i < 128; i++)); do
+  for ((i = 1; i < 1000; i++)); do
     # delay between each print in seconds
-    sleep .1
+    sleep .03
     if [ $switch -eq 0 ]; then
-      echo "gay"
+      echo "$2"
     else 
       echo
     fi
@@ -82,10 +86,7 @@ cycle() {
       fi
     fi 
   done
-  white_bold 
-  echo "end"
-  echo
 }
-blue_bold
-cycle 7
+# blue_bold
+# cycle 7 ""
 #================================================== 
---
+++ rosa_menkman_stuff/fractal.py
@@ -0,0 +1,74 @@
+# import sys
+from typing import Tuple#, Dict, Union, Optional, Set
+# import random
+import numpy as np
+#import imageio
+from PIL import Image
+import math
+
+# global parameters
+global_image = Image.open("mandelbrot.png")
+output_path = "output/"
+temp = output_path + "temp.jpg"
+WIDTH, HEIGHT = global_image.size
+
+def get_copy():
+    """
+    return copy of the original image
+    """
+    return global_image.copy()
+
+def mycrop(coordinate: Tuple[int,int], dimension_tuple: Tuple[int,int]):
+    image = get_copy()
+    width, height = dimension_tuple
+    (left, upper) = coordinate
+    (right, lower) = (left + width, upper + height)
+    return_image = global_image.crop((left, upper, right, lower))
+    return return_image
+
+def get_Nth_coordinates(n: int):
+    """
+        meant to take the img global variable and output N images, each being [(x/4 * width) by (x/4 * height) for x from 3-1]
+
+        input: n: the scale. eg 4 means split into 4ths
+        outputs list of images
+    """
+    x_coords = np.linspace(0,WIDTH,n,endpoint=False,dtype=int)
+    y_coords = np.linspace(0,HEIGHT,n,endpoint=False,dtype=int)
+    # list of tuples scaled to the 4th
+    coords = np.rec.fromarrays([x_coords,y_coords]).tolist()
+    return coords
+
+def get_width_and_height_to_crop_from_tuple(tuple: Tuple[int,int]):
+    """
+        to get inputs to feed into
+    """
+    x,y = tuple
+    return (WIDTH-x,HEIGHT-y)
+
+def crop_into_Nths(n=4):
+    """
+        meant to take the img global variable and output N images, each being [(x/4 * width) by (x/4 * height) for x from 3-1]
+
+        outputs list of images
+    """
+    coords = get_Nth_coordinates(n)
+    print(coords)
+    # empty list to store my N images
+    image_list = [global_image.copy()]
+    for coordinate in coords:
+        dimension_tuple = get_width_and_height_to_crop_from_tuple(coordinate)
+        print(f"coord: {coordinate}")
+        print(f"dimension: {dimension_tuple}")
+        cropped_image = mycrop(coordinate,dimension_tuple)
+        image_list.append(cropped_image)
+        cropped_image.show()
+
+    return image_list
+
+# def to_jpeg(image,quality=0):
+#     # https://stackoverflow.com/questions/43258461/convert-png-to-jpeg-using-pillow
+#     rgb_img = img_crop.convert('RGB')
+#     rgb_img.save(temp, quality=0)
+
+list = crop_into_Nths(4)
